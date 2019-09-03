2f6adfa56d0cc259dc0317e50470b95e96e3ab3a
Carlos Mañas <carlos@spacenomads.com>
2019-09-03 17:33:40 +0200

    Improve auto reloading

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,21 @@
 'use strict';
 
-const now = new Date();
-const today = [now.getFullYear(), now.getMonth() + 1, now.getDate()];
-const startDate = moment(today);
-const endDate = moment([2019, 10, 2]);
+
+
 const test = document.querySelector('.js__test');
 const counter = document.querySelector('.js__counter');
-const rDays = endDate.diff(startDate, 'days');
+const reloadInMS = 300000;
+let rDays = getDays();
 let t = 0;
-test.innerHTML = t;
 
-setInterval(function(){
-  t++;
-  test.innerHTML = t;
-}, 5000);
+function getDays() {
+  const now = new Date();
+  const today = [now.getFullYear(), now.getMonth() + 1, now.getDate()];
+  const startDate = moment(today);
+  const endDate = moment([2019, 10, 2]);
+  const days = endDate.diff(startDate, 'days');
+  return days;
+}
 
 function writeRemainingDays(days) {
   let result = '0';
@@ -25,3 +27,10 @@ function writeRemainingDays(days) {
 
 writeRemainingDays(rDays);
 
+const checkDays = setInterval(function(){
+  rDays = getDays();
+  writeRemainingDays(rDays);
+  t++;
+  test.innerHTML = t;
+
+}, reloadInMS);
