8007393c03d6a31b8ebb1ebd0b86a78b73bba41e
Carlos Mañas <carlos@spacenomads.com>
2019-09-03 16:57:35 +0200

    test setInterval sh*t

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,9 +4,16 @@ const now = new Date();
 const today = [now.getFullYear(), now.getMonth() + 1, now.getDate()];
 const startDate = moment(today);
 const endDate = moment([2019, 10, 2]);
-
+const test = document.querySelector('.js__test');
 const counter = document.querySelector('.js__counter');
 const rDays = endDate.diff(startDate, 'days');
+let t = 0;
+test.innerHTML = t;
+
+setInterval(function(){
+  t++;
+  test.innerHTML = t;
+}, 5000);
 
 function writeRemainingDays(days) {
   let result = '0';
